705110454c78716cdcd3c3f1bbf7cd5dd10bce2e
ROFLERGG <alexeysuper0@gmail.com>
2024-07-31 22:09:41 +0300

    first section done

--- meta/src/pages/Home/home.jsx
+++ meta/src/pages/Home/home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
       <div className="flex flex-col">
         {/* section 1 */}
         <Section>
-          <div className="flex justify-center items-center gap-[30px] max-lg:flex-col-reverse">
+          <div className="flex justify-center items-center gap-[30px] max-md:flex-col-reverse">
             {/* left side */}
-            <div className="flex flex-col max-lg:text-center max-lg:items-center space-y-12">
-              <div className="flex flex-col space-y-6 max-lg:items-center">
-                <h1 className="text-5xl font-mont font-semibold tracking-widest whitespace-nowrap">
+            <div className="flex flex-col max-md:text-center max-md:items-center space-y-12">
+              <div className="flex flex-col space-y-6 max-md:items-center">
+                <h1 className="font-mont leading-tight font-semibold tracking-widest whitespace-nowrap">
                   META RECORD <br /> LABEl
                 </h1>
-                <span className="max-w-[360px] font-mont">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</span>
+                <span className="paragraph-md max-w-[360px] font-mont">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</span>
               </div>
               <div className="flex">
                 <button className="px-6 py-4 leading-none bg-green-500 text-neutral-950">Learn more</button>
--- meta/src/pages/Home/section.jsx
+++ meta/src/pages/Home/section.jsx
@@ -1,6 +1,6 @@
 const Section = ({ children }) => {
   return (
-    <section className="py-[60px]">
+    <section className="py-[60px] max-md:py-[30px]">
       <div className="max-w-[1600px] mx-auto px-4">{children}</div>
     </section>
   );
